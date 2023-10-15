9e9d43f4e2e3abfab196903f0745f6f64d44a8f1
laksamanaap <laksamana.arya1412@gmail.com>
2023-10-15 19:38:30 +0700

    feat : setup pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import ForgotPassword from "./pages/ForgotPassword";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 function App() {
   return (
     <>
-      <h1>House Marketplace</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />}></Route>
+          <Route path="/home" element={<Explore />}></Route>
+          <Route path="/offers" element={<Offers />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/sign-in" element={<SignIn />}></Route>
+          <Route path="/sign-up" element={<SignUp />}></Route>
+          <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
