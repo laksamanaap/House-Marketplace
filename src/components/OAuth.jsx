9cf133e30556d90c501b71b87ec040c79cc2d2b4
laksamanaap <laksamana.arya1412@gmail.com>
2023-10-17 23:54:04 +0700

    feat : OAuth login using google

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -5,6 +5,8 @@ import { doc, setDoc, getDoc, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { toast } from "react-toastify";
 import googleIcon from "../assets/svg/googleIcon.svg";
+import githubIcon from "../assets/svg/githubIcon.svg";
+import facebookIcon from "../assets/svg/facebookIcon.svg";
 
 function OAuth() {
   const navigate = useNavigate();
@@ -45,12 +47,46 @@ function OAuth() {
     }
   };
 
+  const onFacebookClick = async () => {
+    toast.error("Error signing in with Facebook!", {
+      position: "top-center",
+      autoClose: 4000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  };
+
+  const onGithubClick = async () => {
+    toast.error("Error signing in with Github!", {
+      position: "top-center",
+      autoClose: 4000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  };
+
   return (
     <div className="socialLogin">
       <p>Sign {location.pathname === "/sign-up" ? "up" : "in"} with</p>
-      <button className="socialIconDiv" onClick={onGoogleClick}>
-        <img src={googleIcon} alt="Google Icon" width={25} />
-      </button>
+      <div className="socialLoginWrapper">
+        <button className="socialIconDiv" onClick={onGoogleClick}>
+          <img src={googleIcon} alt="Google Icon" width={20} />
+        </button>
+        <button className="socialIconDiv" onClick={onFacebookClick}>
+          <img src={facebookIcon} alt="Google Icon" width={30} />
+        </button>
+        <button className="socialIconDiv" onClick={onGithubClick}>
+          <img src={githubIcon} alt="Google Icon" width={30} />
+        </button>
+      </div>
     </div>
   );
 }
