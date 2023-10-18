1c6d069e764ab78adfd595c83667635fc5f28c14
laksamanaap <laksamana.arya1412@gmail.com>
2023-10-18 23:03:33 +0700

    feat : listing item

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -5,34 +5,59 @@ import bathIcon from "../assets/svg/bathtubIcon.svg";
 
 import React from "react";
 
-function ListingItem({ listing, id }) {
+function ListingItem({ listing, id, onDelete }) {
   //   console.log(listing.imageUrls[0]);
   return (
     <li className="categoryListing">
       <Link
         to={`category/${listing.type}/${id}`}
         className="categoryListingLink"
-      />
-      <img
-        src={listing.imageUrls[0]}
-        alt={listing.name}
-        className="categoryListingImg"
-      />
-      <div className="categoryListingDetails">
-        <p className="categoryListingLocation">{listing.location}</p>
-        <p className="categoryListingName">{listing.name}</p>
-        <p className="categoryListingPrice">
-          $
-          {listing.offer
-            ? listing.discountedPrice
-                .toString()
-                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-            : listing.regularPrice
-                .toString()
-                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-          {listing.type === "rent" && " / month"}
-        </p>
-      </div>
+      >
+        <img
+          src={listing.imageUrls[0]}
+          alt={listing.name}
+          className="categoryListingImg"
+        />
+        <div className="categoryListingDetails">
+          <p className="categoryListingLocation">{listing.location}</p>
+          <p className="categoryListingName">{listing.name}</p>
+          <p className="categoryListingPrice">
+            $
+            {listing.offer
+              ? listing.discountedPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+              : listing.regularPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            {listing.type === "rent" && " / month"}
+          </p>
+          <div className="categoryListingInfoDiv">
+            <img src={bedIcon} alt="bed" />
+            <p className="categoryListingInfoText">
+              {listing.bedrooms > 1
+                ? `${listing.bedrooms} Bedrooms`
+                : "1 Bedroom"}
+            </p>
+            <img src={bathIcon} alt="bed" />
+            <p className="categoryListingInfoText">
+              {listing.bathrooms > 1
+                ? `${listing.bathrooms} Bedrooms`
+                : "1 Bedroom"}
+            </p>
+          </div>
+        </div>
+
+        {onDelete && (
+          <DeleteIcon
+            className="removeIcon"
+            fill="red"
+            onClick={() => {
+              onDelete(listing.id, listing.name);
+            }}
+          />
+        )}
+      </Link>
     </li>
   );
 }
